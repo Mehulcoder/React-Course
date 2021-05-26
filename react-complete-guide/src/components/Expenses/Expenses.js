4a74521117cc83fa3c8ff8859baca6cce7e9bbcb
Mehul Chaturvedi <mehul355180@gmail.com>
2021-05-26 22:16:15 +0530

    Refactored

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
-import Card from './Card';
+import Card from '../UI/Card';
 
 // I always return the HTML code to be rendered in the component js files.
 function Expenses(props) {
--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
-
 import './index.css';
 import App from './App';
 
+// This is executed at first
+// It renders the first arg, between the second arg
 ReactDOM.render(<App></App>, document.getElementById('root'));
